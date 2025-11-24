e21dcb8f3a6b01c67a7c620ad3dfd762072fbbe5
Valery <verelly.v@gmail.com>
2025-11-24 14:14:46 +0200

    prepare for render deployment: define service settings at render.yaml

--- app/main.py
+++ app/main.py
@@ -74,4 +74,4 @@ if __name__ == "__main__":
     import os
 
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="127.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
